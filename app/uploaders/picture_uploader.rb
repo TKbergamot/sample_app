9314bb6e2e91648ab83e03b535a3adbfba47d3a7
TKbergamot <berg4m0t@gmail.com>
2020-03-17 14:42:29 +0900

    Fix Syntax error

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,7 +5,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   if Rails.env.production?
-    stogage :fog
+    storage :fog
   else
     storage :file
   end
